92770dd557557a9c530dc06986d9c7e45b5a0964
juziem <lus0800@mail.ru>
2019-05-24 14:25:26 +0700

    div

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string a;
+            a = Console.ReadLine();
+            string b;
+            b = Console.ReadLine();
+
         }
     }
 }
