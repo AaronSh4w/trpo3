b0f268feb605d31a03ce833c863919cac8e733d3
juziem <lus0800@mail.ru>
2019-05-24 14:32:07 +0700

    f+daa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,15 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            
-=======
+
             string a;
             a = Console.ReadLine();
             string b;
             b = Console.ReadLine();
 
->>>>>>> divide
+
         }
     }
 }
